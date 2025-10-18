6e0e88a72c4e6f92784d32cfbb4a2e348c23a751
Andrea Ulicna <andrea@ulicna.com>
2025-10-18 14:00:16 +0200

    C Colleen working (watch out for LF vs CRLF), added .gitignore

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /*
-    Don't panic!
+	Don't panic!
 */
 
 void print_quine(char *s)
 {
-    printf(s, s);
+	printf(s, 10, 9, 34, s);
 }
 
 int main(void)
 {
-    /*
-        42 is the answer to life, the universe, and everything.
-    */
-    char *s = "TBA";
-    print_quine(s);
-    return (0);
+	// 42 is the answer to life, the universe, and everything.
+	char *s = "#include <stdio.h>%1$c%1$c/*%1$c%2$cDon't panic!%1$c*/%1$c%1$cvoid print_quine(char *s)%1$c{%1$c%2$cprintf(s, 10, 9, 34, s);%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$c// 42 is the answer to life, the universe, and everything.%1$c%2$cchar *s = %3$c%4$s%3$c;%1$c%2$cprint_quine(s);%1$c%2$creturn (0);%1$c}%1$c";
+	print_quine(s);
+	return (0);
 }
